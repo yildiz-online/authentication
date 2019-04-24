ca2c247e520278cdd1f7eff29390fee0f6e3012a
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-04-24 11:35:40 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,13 @@ module be.yildizgames.authentication {
 
     requires be.yildizgames.common.logging;
     requires be.yildizgames.common.configuration;
-    requires be.yildizgames.common.configuration.parameter;
     requires be.yildizgames.common.exception;
     requires be.yildizgames.module.database.derby;
     requires be.yildizgames.module.database;
     requires be.yildizgames.module.messaging;
-    requires java.sql;
     requires be.yildizgames.common.authentication;
     requires be.yildizgames.common.model;
     requires be.yildizgames.common.util;
+    
+    requires java.sql;
 }
